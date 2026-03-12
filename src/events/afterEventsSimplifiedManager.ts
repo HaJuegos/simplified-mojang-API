76f8009810de3803bd1cc10241337a0b489d6c44
HaCatto (Ha) <hagatoclips69@gmail.com>
2026-03-12 00:13:25 -0500

    [UPDATE] New event, onPlayerSpawns

--- src/events/afterEventsSimplifiedManager.ts
+++ src/events/afterEventsSimplifiedManager.ts
@@ -10,6 +10,7 @@ import * as mc from "@minecraft/server";
  */
 export class AfterEventsSimplified {
     private entityDieManager: BaseEventManager<mc.EntityDieAfterEvent>;
+    private playerSpawnManager: BaseEventManager<mc.PlayerSpawnAfterEvent>;
 
     /**
      * Eventos que se inicializan cuando la clase es llamada o inicializada.
@@ -17,6 +18,7 @@ export class AfterEventsSimplified {
      */
     constructor () {
         this.entityDieManager = new BaseEventManager<mc.EntityDieAfterEvent>(mc.world.afterEvents.entityDie, "AfterEntityDie");
+        this.playerSpawnManager = new BaseEventManager<mc.PlayerSpawnAfterEvent>(mc.world.afterEvents.playerSpawn, "AfterPlayerSpawn");
     }
 
     /**
@@ -27,6 +29,16 @@ export class AfterEventsSimplified {
      */
     public onEntityDie(callback: (args: mc.EntityDieAfterEvent) => void): void {
         this.entityDieManager.register(callback);
+    }
+
+    /**
+     * Metodo auxiliar que ejecuta los eventos relacionado cuando una entidad muere de forma simplificada.
+     * @param {(args: mc.EntityDieAfterEvent) => void} callback Los argumentos del evento y su logica.
+     * @author HaJuegos - 11-03-2026
+     * @public
+     */
+    public onPlayerSpawns(callback: (args: mc.PlayerSpawnAfterEvent) => void): void {
+        this.playerSpawnManager.register(callback);
     }
 }
 
