a1a5e3bd6147b125be1a191702ff44b2a910d6e5
HaCatto (Ha) <hagatoclips69@gmail.com>
2026-03-13 01:34:09 -0500

    [UPDATE] New simplified methods have been added

--- src/events/worldToolsSimplifiedManager.ts
+++ src/events/worldToolsSimplifiedManager.ts
@@ -13,6 +13,49 @@ class WorldToolsSimplified {
     constructor () { }
 
     /**
+     * Metodo auxiliar que establece una serie de eventos con un pequeño retraso determinado. Luego de terminar el retraso, elimina la tarea para no dejarlo en memoria.
+     * @param {() => void} callback Los eventos en cuestion a ejecutar.
+     * @param {number} ticksDelay Los tiempos en ticks que se tardara para ejecutarse.
+     * @returns {Promise<void>} 
+     * @author HaJuegos - 13-03-2026
+     * @async Es un metodo asincrono, debido a que se limpia despues de la ejecucion.
+     * @public
+     */
+    public async setDelay(callback: () => void, ticksDelay: number): Promise<void> {
+        return new Promise<void>((resolve) => {
+            const currentJob = mc.system.runTimeout(() => {
+                callback();
+
+                mc.system.clearRun(currentJob);
+
+                resolve();
+            }, ticksDelay);
+        });
+    };
+
+    /**
+     * Metodo auxiliar que establece una serie de eventos en concreto que se mantendran en loop.
+     * @param {() => void} callback Los eventos en concreto.
+     * @param {number} loopTicks El numero en ticks que se repetira este loop.
+     * @returns {number} Devuelve el ID de la memoria, del proceso loop creado.
+     * @author HaJuegos - 13-03-2026
+     * @public
+     */
+    public setLoop(callback: () => void, loopTicks: number): number {
+        return mc.system.runInterval(callback, loopTicks);
+    };
+
+    /**
+     * Metodo auxiliar que detiene un proceso looping creado previamente por medio de su ID generado en memoria.
+     * @param {number} runID ID del proceso generado.
+     * @author HaJuegos - 13-03-2026
+     * @public
+     */
+    public stopLoop(runID: number): void {
+        mc.system.clearRun(runID);
+    }
+
+    /**
      * Metodo auxiliar que envia un mensaje global al mundo en formato string o rawmessage.
      * @public
      * @param {(string | mc.RawMessage)} message Mensaje o RawMessage en concreto a enviar. 
